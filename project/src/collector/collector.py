8e71dddc9e26245c8af9f05760774a0e3eda0f7b
Rub3ck0r3 <rubecode11001@gmail.com>
2026-02-20 00:55:18 +0100

    week 0: feat: added tests and other details 20-02-2026

--- project/src/collector/collector.py
+++ project/src/collector/collector.py
@@ -1,4 +1,6 @@
 # class to manage the collector implementation
+# It behaves like a queue of operations to do related to the 
+# db, think it throught
 class Collector():
     def __init__(self) -> None:
         pass
