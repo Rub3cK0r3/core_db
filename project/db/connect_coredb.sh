b3ddc0cbcaeb4b4a465add9eaea655dad79e91ed
Rub3cK0r3 <rubecode11001@gmail.com>
2026-02-19 18:32:08 +0100

    week 0: fix: connection core_db sh file in db/ directory

--- project/db/connect_coredb.sh
+++ project/db/connect_coredb.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-export PGPASSWORD="${DB_PASSWORD}"
-psql -h 127.0.0.1 -p 5432 -U "$DB_USER" -d "$DB_NAME"
+# load .env file
+export $(grep -v '^#' .env | xargs)
+
+# connect to PostgreSQL.. 
+PGPASSWORD="$POSTGRES_PASSWORD" psql -h 127.0.0.1 -U "$POSTGRES_USER" -d "$POSTGRES_DB"
 
 # Author : rub3ck0r3
