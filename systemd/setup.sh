01f2948fd4eecf05c1399b168142c2e8bd8c88a5
Rub3cK0r3 <rubecode11001@gmail.com>
2026-03-28 19:29:19 +0100

    fix(systemd): fix referencing to the correct service

--- systemd/setup.sh
+++ systemd/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 sudo systemctl daemon-reload
-sudo systemctl enable ttrssupdate.service 
-sudo systemctl start ttrssupdate.service
+sudo systemctl enable 3v3nTr4cer.service
+sudo systemctl start 3v3nTr4cer.service
 
 # Author : rub3ck0r3
