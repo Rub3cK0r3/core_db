ff50a0c4a3c17411c78f98030d1064305bb9ae3a
Rub3cK0r3 <rubecode11001@gmail.com>
2026-03-30 14:23:18 +0200

    refactor(installer.sh): created a new installation script to run before executing the whole project

---
+++ installer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Installer script for installing all the dependencies located in this project before running it
+
+######################
+# Author : Rub3ck0r3 #
+######################
